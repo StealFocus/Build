4eee9255a7d272beb48d042818ff8b0abb3780cc
CallumHibbert <callum.hibbert@gmail.com>
2011-11-04 23:37:29 +0000

    Switched AssemblyInfoCommon to be per project (not shared), added /m switch to msbuild call in .cmd file (for multi-threaded builds), enabled multi-threading for MSTest, put a limit on the build time.

--- Source/Artefacts/Example/Source/CommandFiles/Acme.PetShop.Build.cmd
+++ Source/Artefacts/Example/Source/CommandFiles/Acme.PetShop.Build.cmd
@@ -8,11 +8,11 @@ ECHO.
 SET /p buildChoice=Please enter F (for Full build) or Q (for Quick build): 
 IF /i %buildChoice% == f (
 ECHO.
-%windir%\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe ..\Scripts\Acme.PetShop.Build.proj  /consoleloggerparameters:Verbosity=minimal /fileLogger /fileLoggerParameters:LogFile=SolutionBuild.msbuild.log;verbosity=diagnostic
+%windir%\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe ..\Scripts\Acme.PetShop.Build.proj /m /consoleloggerparameters:Verbosity=minimal /fileLogger /fileLoggerParameters:LogFile=SolutionBuild.msbuild.log;verbosity=diagnostic
 ) ELSE (
 	IF /i %buildChoice% == q (
 	ECHO.
-	%windir%\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe ..\Scripts\Acme.PetShop.Build.proj /p:BuildReleaseConfiguration=false;PopulateDropLocationForBuild=false /consoleloggerparameters:Verbosity=minimal /fileLogger /fileLoggerParameters:LogFile=SolutionBuild.msbuild.log;verbosity=diagnostic
+	%windir%\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe ..\Scripts\Acme.PetShop.Build.proj /m /p:BuildReleaseConfiguration=false;PopulateDropLocationForBuild=false /consoleloggerparameters:Verbosity=minimal /fileLogger /fileLoggerParameters:LogFile=SolutionBuild.msbuild.log;verbosity=diagnostic
 	) ELSE (
 		ECHO.
 		ECHO Invalid selection
